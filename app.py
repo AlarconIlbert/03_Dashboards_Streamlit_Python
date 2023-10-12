6b89fc04826d9e34dacc520aac9f4f845d19b437
AlarconIlbert <ilbert.alarcon@outlook.com>
2023-10-11 20:24:52 -0500

    Update app

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.title('Hello World!')
-st.markdown('## My First streamlit dashboard!')
+st.title('Sentimental Analysis of Tweets about US Airlines!')
+st.sidebar.title('Sentimental Analysis of Tweets about US Airlines!')
+
+st.markdown("This application is a Street dashboard to analyze the sentimental of Tweets")
